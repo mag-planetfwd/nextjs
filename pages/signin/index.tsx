0abb6b6443cd4a7c6fe6f9c4fbf4906b5657accd
mag <mag@planetfwd.com>
2023-04-02 07:33:49 -0700

    update image tag

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,12 +1,14 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
 
 export default function Example() {
     return (
         <>
+
             <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
                 <div className="w-full max-w-md space-y-8">
                     <div>
-                        <img
+                        <Image
                             className="mx-auto h-12 w-auto"
                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                             alt="Your Company"
